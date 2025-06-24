b859e09e19e0c7f1c332a68f8789cea0026afd0b
Johan-Potgieter-info <johan.p@live.co.za>
2025-06-24 16:32:52 +0000

    fix: Correctly update consentAgreement checkbox value in onCheckboxChange handler

--- src/components/ConsentSection.tsx
+++ src/components/ConsentSection.tsx
@@ -67,7 +67,7 @@ const ConsentSection: React.FC<ConsentSectionProps> = ({
     
     // Update main form data (fix the issue)
     if (onCheckboxChange) {
-      onCheckboxChange('consentAgreement', 'true', newConsentValue);
+      onCheckboxChange('consentAgreement', newConsentValue.toString(), newConsentValue);
     }
   };
 

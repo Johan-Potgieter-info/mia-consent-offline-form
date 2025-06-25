083d3f01a970061393853143e58a4e89b728d837
Johan-Potgieter-info <johan.p@live.co.za>
2025-06-25 12:28:33 +0000

    loading fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import FormPage from "./pages/FormPage";
-import DraftList from "./pages/DraftList";
-import DraftEditor from "./pages/DraftEditor";
-import NotFound from "./pages/NotFound";
 
-const App: React.FC = () => (
-  <Router basename="/mia-consent-offline-form">
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/consent-form" element={<FormPage />} />
-      <Route path="/consent-form/:draftId" element={<FormPage />} />
-      <Route path="/drafts" element={<DraftList />} />
-      <Route path="/edit/:id" element={<DraftEditor />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>
-);
+const App: React.FC = () => {
+  console.log("🧪 Simple render test working");
+
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>✅ App Rendered</h1>
+      <p>If you see this, the error is inside one of the routes.</p>
+    </div>
+  );
+};
 
 export default App;

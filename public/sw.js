b0f868d12e49a2403ac738c756e863628bb20d9d
Johan-Potgieter-info <johan.p@live.co.za>
2025-07-06 19:19:34 +0000

    🔁 Replace SW to force cache bust and update

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  console.log('[SW] Installing – clearing old cache');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[SW] Activating – cleaning all caches');
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
